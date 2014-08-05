fde9b1bc8af7de7ec5172451c130409a23c7c2b0
Lorenzo Pisani <zeelot3k@gmail.com>
2014-08-04 18:53:18 -0700

    fix schema to use unsigned when it makes sense

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE  TABLE `prices` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `stock_id` INT NOT NULL ,
+  `stock_id` INT UNSIGNED NOT NULL ,
   `date` date NOT NULL ,
   `closing_price` double(10,4) UNSIGNED NOT NULL ,
   `change` double(14,8) NOT NULL DEFAULT 0 ,
@@ -9,7 +9,7 @@ CREATE  TABLE `prices` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `stocks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `symbol` varchar(50) NOT NULL,
   `company` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -17,9 +17,9 @@ CREATE TABLE `stocks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `reports` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
-  `duration` int(11) NOT NULL,
+  `duration` INT UNSIGNED NOT NULL,
   `formula` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_report` (`date`, `duration`, `formula`)
@@ -27,8 +27,8 @@ CREATE TABLE `reports` (
 
 CREATE TABLE `ratios` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `stock_id` int(11) NOT NULL,
-  `report_id` int(11) NOT NULL,
+  `stock_id` INT UNSIGNED NOT NULL,
+  `report_id` INT UNSIGNED NOT NULL,
   `ratio` double(10,6),
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_stock_report` (`stock_id`, `report_id`),
@@ -37,22 +37,22 @@ CREATE TABLE `ratios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `picks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `recipe_id` int(11) NOT NULL,
-  `report_id` int(11) NOT NULL,
-  `stock_id` int(11) NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `recipe_id` INT UNSIGNED NOT NULL,
+  `report_id` INT UNSIGNED NOT NULL,
+  `stock_id` INT UNSIGNED NOT NULL,
   `gain` double(10,6),
-  `weight` double(10,6),
+  `weight` double(10,6) UNSIGNED,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_recipe_stock` (`recipe_id`, `stock_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `recipes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `n_stocks` int(11) NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `n_stocks` INT UNSIGNED NOT NULL,
   `check_correlation` tinyint(1) UNSIGNED NOT NULL,
   `distribution` varchar(10),
-  `report_duration` int(11) NOT NULL,
+  `report_duration` INT UNSIGNED NOT NULL,
   `report_formula` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
