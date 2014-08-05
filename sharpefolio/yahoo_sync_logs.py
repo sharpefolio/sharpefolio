802ad65e6b6d10289509cee104dd26b725e99a6f
Lorenzo Pisani <zeelot3k@gmail.com>
2014-08-05 00:31:18 -0700

    finish up needed functionality for the yahoo sync logs

--- sharpefolio/yahoo_sync_logs.py
+++ sharpefolio/yahoo_sync_logs.py
@@ -3,7 +3,7 @@ import MySQLdb
 import datamapper as dm
 
 class YahooSyncLog(object):
-    def __init__(self, stock_id, year, month, is_successful, log, id = None):
+    def __init__(self, stock_id, year, month, is_successful, log = {'errors': []}, id = None):
         self.id = id
         self.stock_id = stock_id
         self.year = year
@@ -11,11 +11,29 @@ class YahooSyncLog(object):
         self.is_successful = is_successful
         self.log = log
 
+    def append_error_log(self, error_log, set_failed = True):
+        self.log['errors'].append(error_log)
+        if set_failed:
+            self.is_successful = False
+
+
 class YahooSyncLogMapper(dm.Mapper):
+    def save(self, model):
+        self._repository.save(model)
+
     def insert(self, model):
         self._repository.insert(model)
 
+    def find_by_stock_year_and_month(self, stock_id, year, month):
+        return self._repository.find_by_stock_year_and_month(stock_id, year, month)
+
 class YahooSyncLogMysqlRepository(dm.MysqlRepository):
+    def save(self, model):
+        if model.id == None:
+            return self.insert(model)
+        else:
+            return self.update(model)
+
     def insert(self, model):
         cursor = self._database.cursor()
         cursor.execute('\
@@ -31,3 +49,53 @@ class YahooSyncLogMysqlRepository(dm.MysqlRepository):
             )
         )
         self._database.commit()
+
+    def update(self, model):
+        cursor = self._database.cursor()
+        cursor.execute('\
+            UPDATE `yahoo_sync_logs`\
+            SET\
+                `stock_id` = %s,\
+                `year` = %s,\
+                `month` = %s,\
+                `is_successful` = %s,\
+                `log` = %s\
+            WHERE `id` = %s',
+            (
+                model.stock_id,
+                model.year,
+                model.month,
+                int(model.is_successful),
+                json.dumps(model.log, indent = 4, separators = (',', ': ')),
+                model.id
+            )
+        )
+        self._database.commit()
+
+    def find_by_stock_year_and_month(self, stock_id, year, month):
+        cursor = self._database.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('\
+            SELECT * FROM `yahoo_sync_logs`\
+            WHERE `stock_id` = %s\
+            AND `year` = %s\
+            AND `month` = %s\
+            LIMIT 1',
+            (
+                stock_id,
+                year,
+                month
+            )
+        )
+
+        # Use the collection class for datamaps but return the first result.
+        collection = dm.Collection(YahooSyncLog, cursor, self._datamap)
+        for item in collection:
+            return item
+
+    def _datamap(self, data):
+        data['is_successful'] = int(data['is_successful'])
+        data['year'] = int(data['year'])
+        data['month'] = int(data['month'])
+        data['is_successful'] = data['is_successful'] == 1
+        data['log'] = json.loads(data['log'])
+        return data
